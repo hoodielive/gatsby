1a643be2b4d1c5ea6b61a41891e038c469bd75af
hoodielive <hoodielive@gmail.com>
2019-10-01 21:27:22 -0400

    about us

---
+++ ofunbala/src/pages/about.js
@@ -0,0 +1,8 @@
+import React from "react" 
+
+export default () => (
+	<div style={{ color: `teal` }} 
+		<h1> About Ofun! </h1>
+		<p> This is dope! </p>
+	</div>
+)
--- ofunbala/src/pages/index.js
+++ ofunbala/src/pages/index.js
@@ -1,21 +1,9 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from "react" 
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
-
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to Ofun Bala</p>
-    <p>Saint conquers The Devil, Buddha will save you from sin.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+export default () => (
+	<div style={{ color: `teal`, fontSize: `62px` }}> 
+		<h1> Howdy Ofun! </h1>
+		<p> What in the world? </p>
+		<img src="https://source.unsplash.com/random/400x200" alt="" />
+	</div> 
 )
-
-export default IndexPage
